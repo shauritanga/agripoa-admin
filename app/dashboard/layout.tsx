3f484c902daeb0669ee6ff60a12e8d7a6f44f06b
shauritanga <curtisisaac36@gmail.com>
2025-05-01 17:15:56 +0300

    fixing hang login flow

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 
-import { Inter } from "next/font/google";
 import { Sidebar } from "../components/Sidebar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Avocado Cooperative",
